ce9037c903ea8689c006741ed7991cfc5e49c082
mdricard <ricard@uta.edu>
2021-09-27 15:41:58 -0500

    test my_function a new feature

--- main.py
+++ main.py
@@ -8,11 +8,15 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hello, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+def my_function(a, b):
+    return a + b
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Homer J Simpson')
     print('Eating too many doughnuts will make you fat, you need some exercise')
     print('Lisa its your birthday, happy birthday lisa')
+    print(my_function(2, 5))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
